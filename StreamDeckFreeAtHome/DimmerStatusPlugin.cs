34274f61683dc9564ceac1f9263ab47c798810be
Philip Gerke <p.gerke@online.de>
2021-04-05 21:05:04 +0200

    feat: Implement dimmer
    PG-89

--- StreamDeckFreeAtHome/DimmerStatusPlugin.cs
+++ StreamDeckFreeAtHome/DimmerStatusPlugin.cs
@@ -12,29 +12,33 @@ using Websocket.Client;
 namespace PhilipGerke.StreamDeckFreeAtHome
 {
     /// <summary>
-    ///     A stream deck action that shows the state of a binary switch and allows to toggle it.
+    ///     A stream deck action that shows the state of a dimmer and allows to toggle it.
     /// </summary>
-    [PluginActionId("com.philipgerke.freeathome.binaryswitch")]
-    public sealed class BinarySwitchPlugin : WebSocketPlugin
+    [PluginActionId("com.philipgerke.freeathome.dimmerstatus")]
+    public sealed class DimmerStatusPlugin : WebSocketPlugin
     {
         private const string PathDatapoints = "00000000-0000-0000-0000-000000000000.datapoints";
         private const string PathHttpResponse = "00000000-0000-0000-0000-000000000000.values[0]";
+        private uint currentState;
+        private float currentBrightness = 0f;
 
         /// <summary>
-        ///     Constructs a new <see cref="BinarySwitchPlugin"/> instance.
+        ///     Constructs a new <see cref="DimmerStatusPlugin"/> instance.
         /// </summary>
         /// <param name="connection">The Stream Deck connection.</param>
         /// <param name="payload">The initial payload.</param>
-        public BinarySwitchPlugin(ISDConnection connection, InitialPayload payload)
+        public DimmerStatusPlugin(ISDConnection connection, InitialPayload payload)
             : base(connection, payload)
         {
+            currentState = payload.State;
             GetInitialData().ConfigureAwait(true);
-        }        
+        }
 
         /// <inheritdoc/>
         public override async void KeyPressed(KeyPayload payload)
         {
             base.KeyPressed(payload);
+            currentState = (currentState + 1) % 3;
             async Task SetSwitchState(bool enabled)
             {
                 HttpContent httpContent = new StringContent(enabled ? "1" : "0", null, "text/plain");
@@ -50,15 +54,15 @@ namespace PhilipGerke.StreamDeckFreeAtHome
             switch (payload.State)
             {
                 case 1:
-                    Logger.Instance.LogMessage(TracingLevel.ERROR, "Switching on");
+                    Logger.Instance.LogMessage(TracingLevel.DEBUG, "Switching on");
                     await SetSwitchState(true);
                     return;
                 case 2:
-                    Logger.Instance.LogMessage(TracingLevel.ERROR, "Switching on");
+                    Logger.Instance.LogMessage(TracingLevel.DEBUG, "Switching on");
                     await SetSwitchState(false);
                     return;
                 default:
-                    Logger.Instance.LogMessage(TracingLevel.ERROR, "State is unknown!");
+                    Logger.Instance.LogMessage(TracingLevel.DEBUG, "State is unknown!");
                     await Connection.ShowAlert();
                     await GetInitialData();
                     return;
@@ -72,11 +76,13 @@ namespace PhilipGerke.StreamDeckFreeAtHome
             GetInitialData().ConfigureAwait(true);
         }
 
-        private async Task GetInitialData()
+        private async Task GetInitialData() => await Task.WhenAll(GetInitialDataBrightness(), GetInitialDataPowerState());
+
+        private async Task GetInitialDataBrightness()
         {
             // Nothing to do if host name, device ID or channel are not set.
             if (string.IsNullOrWhiteSpace(Settings.SysApIpAddress)
-                || string.IsNullOrWhiteSpace(Settings.DeviceId) 
+                || string.IsNullOrWhiteSpace(Settings.DeviceId)
                 || string.IsNullOrWhiteSpace(Settings.Channel))
             {
                 Logger.Instance.LogMessage(TracingLevel.WARN, "Cannot process message as mandatory device and channel data is missing.");
@@ -84,10 +90,10 @@ namespace PhilipGerke.StreamDeckFreeAtHome
             }
 
             HttpResponseMessage response = await httpClient.GetAsync(
-                $"http://{Settings.SysApIpAddress}/fhapi/v1/api/rest/datapoint/{Guid.Empty}/{Settings.DeviceId}.{Settings.Channel}.odp0000");
+                $"http://{Settings.SysApIpAddress}/fhapi/v1/api/rest/datapoint/{Guid.Empty}/{Settings.DeviceId}.{Settings.Channel}.odp0001");
             if (!response.IsSuccessStatusCode)
             {
-                Logger.Instance.LogMessage(TracingLevel.ERROR, "Getting initial data failed: " + await response.Content.ReadAsStringAsync());
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Getting initial power state data failed: " + await response.Content.ReadAsStringAsync());
                 await Connection.ShowAlert();
                 return;
             }
@@ -98,13 +104,48 @@ namespace PhilipGerke.StreamDeckFreeAtHome
             JToken token = await JToken.ReadFromAsync(jsonReader);
 
             // Extract and process value
-            if(token.SelectToken(PathHttpResponse) is not JValue value || value == null)
+            if (token.SelectToken(PathHttpResponse) is not JValue value || value == null)
             {
                 Logger.Instance.LogMessage(TracingLevel.ERROR, "Cannot process response to HTTP request.");
                 return;
             }
 
-            await ProcessValue(value.Value<int>());
+            await ProcessBrightness(value.Value<float>());
+        }
+
+        private async Task GetInitialDataPowerState()
+        {
+            // Nothing to do if host name, device ID or channel are not set.
+            if (string.IsNullOrWhiteSpace(Settings.SysApIpAddress)
+                || string.IsNullOrWhiteSpace(Settings.DeviceId)
+                || string.IsNullOrWhiteSpace(Settings.Channel))
+            {
+                Logger.Instance.LogMessage(TracingLevel.WARN, "Cannot process message as mandatory device and channel data is missing.");
+                return;
+            }
+
+            HttpResponseMessage response = await httpClient.GetAsync(
+                $"http://{Settings.SysApIpAddress}/fhapi/v1/api/rest/datapoint/{Guid.Empty}/{Settings.DeviceId}.{Settings.Channel}.odp0000");
+            if (!response.IsSuccessStatusCode)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Getting initial power state data failed: " + await response.Content.ReadAsStringAsync());
+                await Connection.ShowAlert();
+                return;
+            }
+
+            // Deserialize message
+            using StreamReader streamReader = new(await response.Content.ReadAsStreamAsync());
+            using JsonReader jsonReader = new JsonTextReader(streamReader);
+            JToken token = await JToken.ReadFromAsync(jsonReader);
+
+            // Extract and process value
+            if (token.SelectToken(PathHttpResponse) is not JValue value || value == null)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Cannot process response to HTTP request.");
+                return;
+            }
+
+            await ProcessState(value.Value<int>());
         }
 
         /// <inheritdoc/>
@@ -137,34 +178,62 @@ namespace PhilipGerke.StreamDeckFreeAtHome
 
             // Check if relevant datapoints are included
             JProperty datapoint = datapoints.Children().FirstOrDefault(e => e.Type == JTokenType.Property &&
-                    (e as JProperty).Name.Equals($"{Settings.DeviceId}/{Settings.Channel}/odp0000", StringComparison.InvariantCultureIgnoreCase)) as JProperty;
+                    ((e as JProperty).Name.Equals($"{Settings.DeviceId}/{Settings.Channel}/odp0000", StringComparison.InvariantCultureIgnoreCase)
+                    || (e as JProperty).Name.Equals($"{Settings.DeviceId}/{Settings.Channel}/odp0001", StringComparison.InvariantCultureIgnoreCase))) as JProperty;
             if (datapoint == null)
             {
                 Logger.Instance.LogMessage(TracingLevel.DEBUG, "The message contains no relevant data points.");
                 return;
             }
 
-            await ProcessValue(datapoint.First.Value<int>());
+            if (datapoint.Name.EndsWith("/odp0000"))
+            {
+                await ProcessState(datapoint.First.Value<int>());
+            }
+            else if (datapoint.Name.EndsWith("/odp0001"))
+            {
+                await ProcessBrightness(datapoint.First.Value<float>());
+            }
+            else
+            {
+                Logger.Instance.LogMessage(TracingLevel.DEBUG, "The data point name is unexpected.");
+            }
         }
 
-        private async Task ProcessValue(int value)
+        private async Task ProcessBrightness(float brightness)
         {
-            switch (value)
+            currentBrightness = brightness;
+            if (currentState != 2)
+                return;
+
+            await Connection.SetTitleAsync($"{brightness!:0}%");
+            Logger.Instance.LogMessage(TracingLevel.INFO, $"{Settings.DeviceId} {Settings.Channel} brightness is now at {brightness:0}%.");
+        }
+
+        private async Task ProcessState(int state)
+        {
+            switch (state)
             {
                 case 0:
+                    currentState = 1;
                     await Connection.SetStateAsync(1);
+                    await Connection.SetTitleAsync("Off");
                     Logger.Instance.LogMessage(TracingLevel.INFO, $"{Settings.DeviceId} {Settings.Channel} is now off.");
                     break;
                 case 1:
+                    currentState = 2;
                     await Connection.SetStateAsync(2);
+                    await Connection.SetTitleAsync($"{currentBrightness!:0}%");
                     Logger.Instance.LogMessage(TracingLevel.INFO, $"{Settings.DeviceId} {Settings.Channel} is now on.");
                     break;
                 default:
+                    currentState = 0;
                     await Connection.SetStateAsync(0);
+                    await Connection.SetTitleAsync(string.Empty);
                     await Connection.ShowAlert();
                     Logger.Instance.LogMessage(TracingLevel.ERROR, $"{Settings.DeviceId} {Settings.Channel} has an unknown value.");
                     break;
             }
-        }        
+        }
     }
 }
